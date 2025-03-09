4a2df4084f482fe4ca5af1d42e9fdeab63051698
milanperera1993 <milanperera1993@gmail.com>
2025-03-09 10:36:36 +0530

    ehancements to manual chunk building

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
 
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.split('node_modules/')[1].split('/')[0];
+        manualChunks(id: string): string | undefined {
+          if (id.includes("node_modules")) {
+            try {
+              const cleanPath = id.split("?")[0];
+
+              const sanitizedPath = cleanPath.replace(/\0/g, "");
+
+              const filePath = path.normalize(sanitizedPath);
+
+              const stats = fs.statSync(filePath);
+              const sizeInKB = stats.size / 1024;
+              if (sizeInKB > 300) return "large-vendors";
+              if (sizeInKB > 50) return "medium-vendors";
+              return "small-vendors";
+            } catch (e) {
+              console.error(`Error reading file: ${id}`, e);
+              return "vendors";
+            }
           }
-        }
-      }
+        },
+      },
     },
-    chunkSizeWarningLimit: 600, 
-  }
-})
+    chunkSizeWarningLimit: 1000,
+  },
+});

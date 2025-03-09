eab08f7d1e193f6ea09f21dabee2e1e16364a9d9
milanperera1993 <milanperera1993@gmail.com>
2025-03-09 10:42:53 +0530

    clear

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: string): string | undefined {
-          if (id.includes("node_modules")) {
-            try {
-              const cleanPath = id.split("?")[0];
-
-              const sanitizedPath = cleanPath.replace(/\0/g, "");
-
-              const filePath = path.normalize(sanitizedPath);
-
-              const stats = fs.statSync(filePath);
-              const sizeInKB = stats.size / 1024;
-              if (sizeInKB > 300) return "large-vendors";
-              if (sizeInKB > 50) return "medium-vendors";
-              return "small-vendors";
-            } catch (e) {
-              console.error(`Error reading file: ${id}`, e);
-              return "vendors";
-            }
-          }
-        },
-      },
-    },
     chunkSizeWarningLimit: 1000,
   },
+
 });
